7640ae202f89b89e57e6f42bf030ffb5ba11e080
Sadie Edwards <91231362+SadieEdwar@users.noreply.github.com>
2023-10-20 22:42:46 -0400

    Upload of Q14

--- lab1_q14_816019835.c
+++ lab1_q14_816019835.c
@@ -64,56 +64,64 @@
 #pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0007FFh) not protected from table reads executed in other blocks)
 
 
-#include <xc.h>
-#include <stdio.h>
-#include <time.h>
+#include <xc.h> //Access to Registers bits
+#include <stdio.h> //Use input and output functions
+#include <time.h> //Access time structures 
 
 /* Demo table-based cyclic task dispatcher for Linux with
 multiple task slots */
-#define CLOCKS_PER_SECOND 8000000
-#define SLOTX 4
-#define CYCLEX 5
+#define CLOCKS_PER_SECOND 8000000 //oscillator frequency
+#define SLOTX 4 //function table size
+#define CYCLEX 5 //function table size
 #define SLOT_T 5000 // 5 sec slot time
 // gcc cyclicx.c -o cyclicx
 int cycle=0, slot=0;
-time_t tick;
+time_t tick; 
 clock_t now, then;
 struct tm n;
 
-void putch(char c)
+void putch(char c) //location for stdout to for outputting each character using printf()) via USART
 {
-    while(!TXIF)
+    while(!TXIF) //Send data to EUSART receive buffer once not empty
         continue;
-    TXREG = c;
+    TXREG = c; //Parsing the data to be printed to the Read/Write Transmit Buffer register
    
+}
+
+char getch_o(void)
+{
+    while(!PIR1bits.RCIF) //Checks if the EUSART Receive buffer, RCREG is full via the interrupt flags being enabled 
+        continue;
+    return RCREG; //The character from the user is stored and returned to the EUSART receive buffer
 }
 
 void USARTsetup(void)
 {
     TRISCbits.TRISC6 = 1;
     TRISCbits.TRISC7 = 0;
-    RCSTAbits.SPEN = 1;
-    TXSTAbits.BRGH = 1;
-    TXSTAbits.SYNC = 0;
-    TXSTAbits.TXEN = 1;
-    SPBRGH = 0x00;
-    SPBRG = 0b00011001;
+    RCSTAbits.SPEN = 1; //receive status and control register bit 7 (SPEN), serial port enabling RX/DT and TX/CK pins as serial port pins
+    RCSTAbits.CREN = 1; // enable continuous receive bit
+    TXSTAbits.BRGH = 1; // Selects High baud rate speed
+    TXSTAbits.SYNC = 0; // EUSART asynchronous mode select bit
+    TXSTAbits.TXEN = 1; // transmit status and control register
+    SPBRGH = 0x00; //EUSART baud rate generator register high byte
+    SPBRG = 0b00011001; //Setting the baud rate of 19230 for FOSC= 8.00MHz for asynchronous mode
     BAUDCON = 0b00000000;
 }
 
 void timer0setup(void)
 {
-    T0CON = 0b10000010;
-    INTCONbits.GIE = 1;
-    INTCONbits.TMR0IE = 1;
+    T0CON = 0b10000010; //A prescaler of 8 is used and the Timer0 is turned On
+    INTCONbits.GIE = 1; //Enable all high priority interrupts
+    INTCONbits.TMR0IE = 1; //Enables the TMR0 overflow interrupt
 }
 
 void __interrupt(high_priority) tcint(void)
 {
-    if (INTCONbits.TMR0IE && INTCONbits.TMR0IF)
+    if (INTCONbits.TMR0IE && INTCONbits.TMR0IF) //Checks if the TMR0 register has overflowed once the TMR0 overflow interrupt is enabled
     {
-        INTCONbits.TMR0IF = 0;
-        tick++;
+        INTCONbits.TMR0IF = 0; //clear the TMR0 register overflow interrupt flag
+        tick++; //A counter to keep track each 250ms passing
     }
     
     return;
@@ -122,45 +130,45 @@ void __interrupt(high_priority) tcint(void)
 void sleep(int sec)
 {
     now = tick;
-    while((tick-now)< (sec*4));
+    while((tick-now)< (sec*4));// waits for the seconds passed to the function to pass
 }
 
 void one()                                       // task code
 {
-    printf("task 1 running\n");
-    sleep(1);
+    printf("task 1 running\n"); 
+    sleep(1); //wait for 1 second
 }
 void two() 
 {
     printf("task 2 running\n");
-    sleep(2);
+    sleep(2); //wait for 2 seconds
 }
 void three() 
 {
     printf("task 3 running\n");
-    sleep(3);
+    sleep(3); //wait for 3 seconds
 }
 void four() {
     printf("task 4 running\n");
-    sleep(4);
+    sleep(4); //wait for 4 seconds
 }
 void five() 
 {
     printf("task 5 running\n");
-    sleep(5);
+    sleep(5); //wait for 5 seconds
 }
 void burn() 
 {
-    clock_t bstart = tick;
+    clock_t bstart = tick; //stores the counter in the clock structure that measures processor and CPU time.
     while (tick < 20) {
-            /* burn time here */
+            /* burn time for 5 seconds here */
     }
-    tick = 0;
+    tick = 0; //resets the time that has passed since completing each function
     printf("burn time = %2.2ldms\n\n", (20-bstart)*
-    250);
-    cycle = CYCLEX;
+    250); //prints the time it took between completing functions
+    cycle = CYCLEX; //restarts the cycling through of arrays
 }
-void (*ttable[SLOTX] [CYCLEX]) () = 
+void (*ttable[SLOTX] [CYCLEX]) () = //2 dimensional function pointers to the functions being called in the function arrays
 {
     {one, two, burn, burn, burn},
     {one, three, burn, burn, burn}, 
@@ -169,15 +177,21 @@ void (*ttable[SLOTX] [CYCLEX]) () =
 };
 void main(void) 
 {
-    timer0setup();
-    USARTsetup();
-    int tps = 4;
+    char result;
+    result = getch_o();
+    
+    printf("%c", result);
+    
+    timer0setup(); //Function to enable hardware Timer0
+    USARTsetup(); //Enable transmitting of data between the chip and keyboard
+    int tps = 4; 
     printf("clock ticks/sec = %d\n\n", tps);
-    while (1) {
+    while (1) //cycling through each element of the function pointer table created
+    {
     for(slot=0; slot<SLOTX; slot++)
     for(cycle=0; cycle<CYCLEX; cycle++)
     (*ttable[slot] [cycle]) (); // dispatch next task
                     // from table
-}
+    }
 return;
 }	
